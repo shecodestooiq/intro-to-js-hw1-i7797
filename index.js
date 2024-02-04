e9b805b543d66562907079486787d758cf40405a
nura <nooraliabdalhossen@gmail.com>
2024-02-04 20:36:49 +0300

    did hw

---
+++ index.js
@@ -0,0 +1,27 @@
+var myName='noura';
+var myAge=25;
+var isStudent= true;
+var number1=10;
+var number2=5;
+
+console.log(' myName:',myName)
+console.log('myAge:', myAge )
+console.log('isStudent :',isStudent )
+
+console.log('type of myName:',typeof myName)
+console.log('type of myAge:', typeof myAge )
+console.log('type of isStudent :', typeof isStudent )
+
+console.log( 'nu1 :',number1 ,'nu2:', number2)
+
+console.log('sum :',number1+number2)
+console.log('division:',number1/number2)
+console.log('multip:',number1*number2)
+console.log('subs:',number1-number2)
+console.log('nu1 is greater than nu2 :',number1>number2)
+console.log('nu1 is less than nu2 :',number1<number2)
+console.log('nu1 is eaqual nu2:',number1==number2)
+console.log(' nu1 greater or equal nu2 :',number1>=number2)
+console.log('And :',number1>number2 && number2==5)
+console.log('Or :',number1>number2 || number2==5)
+console.log('Not :',!(number1==10))
